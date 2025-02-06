c3bfba6699731819058696653237cadddbb6c15a
Kingsley Ugwudinso <kingholyhill@gmail.com>
2025-02-06 11:02:48 +0100

    website verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,31 @@
-import './globals.css';
-import Navbar from './components/Navbar/index';
-import Footer from './components/Footer/Footer';
-
+import "./globals.css";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/Footer";
 
 export const metadata = {
-  title: 'Veracone',
-  description: 'Improved Learning, Better Academic Performance using Prepmate powered by Veracone Technologies',
-}
+  title: "Veracone",
+  description:
+    "Improved Learning, Better Academic Performance using Prepmate powered by Veracone Technologies",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="ATsY4ZF8FViyNDri5OBOCP2y9Cqt-5rLy-LCynivq1k"
+        />
+      </head>
       <body>
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
